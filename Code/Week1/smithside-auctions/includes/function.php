34669ddc7be1af3132465066dc3b20c10b39991d
DESKTOP-6RB3LQV\suimi <suimieya@gmail.com>
2018-04-19 22:13:24 -0400

    *

--- Code/Week1/smithside-auctions/includes/function.php
+++ Code/Week1/smithside-auctions/includes/function.php
@@ -9,7 +9,7 @@
 function loadContent($where, $default=''){
     // get the content from the url
     // sanitize it for security reasons
-    $_GET;
+    
     $content = filter_input(INPUT_GET, $where, FILTER_SANITIZE_STRING);
     $default = filter_var($default, FILTER_SANITIZE_STRING);
     // the there wasn't anything on the url, then use the default
--- Sources/smithside-auctions/content/catnav.php
+++
@@ -1,6 +0,0 @@
-<h3 class="element-invisible">Lot Categories</h3>
-<ul class="catnav">
-    <li><a href="index.php?content=gents">Gents</a></li>	
-    <li><a href="index.php?content=sporting">Sporting</a></li>	
-    <li><a href="index.php?content=women">Women</a></li>
-</ul>
